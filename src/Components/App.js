b7551a32ac53ad2dfeda0bc91eda893e6258a0ed
Alona Varshal <thirdwave72@gmail.com>
2018-05-02 16:47:46 -0500

    Create singlemovie container

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const App = () => (
-  <div><h1>HELLO!</h1></div>
-)
+import SingleMovie from './SingleMovie';
 
-export default App
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Route path="/" component={SingleMovie} />
+      </div>
+    </Router>
+  );
+};
+export default App;
