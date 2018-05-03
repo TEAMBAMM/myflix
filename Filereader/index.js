223581fab9247c5c1e57fc57f00410ad0e157969
Blake Zeiger <blakezeiger@gmail.com>
2018-05-03 18:06:55 -0400

    added helper function to extract the file name from the path

--- Filereader/index.js
+++ Filereader/index.js
@@ -17,22 +17,27 @@ console.log('Watching: ', url.format({
 }))
 
 fileWatcher.on('add', path => {
+  const name = extractName(path)
   console.log(`Added: ${path}`)
 })
 
 fileWatcher.on('addDir', path => {
+  const name = extractName(path)
   console.log(`Added: ${path}`)
 })
 
 fileWatcher.on('change', path => {
+  const name = extractName(path)
   console.log(`Changed: ${path}`)
 })
 
 fileWatcher.on('unlink', path => {
+  const name = extractName(path)
   console.log(`Removed: ${path}`)
 })
 
 fileWatcher.on('unlinkDir', path => {
+  const name = extractName(path)
   console.log(`Removed: ${path}`)
 })
 
@@ -40,6 +45,10 @@ fileWatcher.on('error', error => {
   console.log(`ERROR: ${error}`)
 })
 
-// const extractName = path => {
-//   for
-// }
+const extractName = path => {
+  for(let i = path.length - 1; i > 0; i--) {
+    if (path[i] === '/' || path[i] === '\\') {
+      return path.slice(i + 1)
+    }
+  }
+}
