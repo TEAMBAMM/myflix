a7b497cd7264cb3effc2be3fe139641096403e97
Blake Zeiger <blakezeiger@gmail.com>
2018-05-04 12:54:22 -0400

    working on network testing

--- server/findservers.js
+++ server/findservers.js
@@ -1,2 +1,33 @@
 const LAN = require('./LAN.js')
-const axios = require('axios')
+const http = require('http')
+
+console.log(LAN)
+const allDevices = []
+
+const options = {
+  host: '192.168.1.12',
+  port: 9000,
+  method: 'GET',
+  path: '/isserver'
+}
+
+const postData = {
+  'msg': 'Hello World!'
+};
+
+setTimeout(() => {
+  console.log('here')
+  
+  http.get({ hostname: '192.168.1.12', port: 9000, path: '/isserver'}, (res) => {
+    let data
+    res.on('data', (chunk) => {
+      data = chunk
+    });
+    res.on('end', () => {
+      console.log(data);
+    });
+  })
+    .on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+}, 1000)
--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const PORT = 9000
 require('./findservers.js')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(morgan('dev'))
 
 app.use(express.static(path.join(__dirname, '../','/movies')))
 
 app.get('/isserver', (req, res, next) => {
-  res.status(200).send({ msg: 'connected' })
+  res.status(200).json({ msg: 'connected' })
 })
 
 app.listen(PORT, () => {
