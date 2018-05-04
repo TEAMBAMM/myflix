af88fdb371ae0e37560500560316375115c1d9c3
Blake Zeiger <blakezeiger@gmail.com>
2018-05-04 11:45:46 -0400

    added morgan

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const morgan = require('morgan')
 const PORT = 9000
+
+app.use(morgan('dev'))
 
 app.use(express.static(path.join(__dirname, '../','/movies')))
 
