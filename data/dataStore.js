ecc99269d94f90af026f55597460a3ae1ed944ab
Matthew Stirnweiss <mpstirn@outlook.com>
2018-05-04 21:13:17 -0400

    Database updates on new files in watched directory

--- data/dataStore.js
+++ data/dataStore.js
@@ -5,37 +5,10 @@ const db = new Datastore({
   autoload: true
 });
 
-const testMovie = {
-  title: 'The Shawshank Redemption',
-  _year_data: '1994',
-  year: 1994,
-  rated: 'R',
-  released: '1994-10-14T04:00:00.000Z',
-  runtime: '142 min',
-  genres: 'Crime, Drama',
-  director: 'Frank Darabont',
-  writer:
-    'Stephen King (short story "Rita Hayworth and Shawshank Redemption"), Frank Darabont (screenplay)',
-  actors: 'Tim Robbins, Morgan Freeman, Bob Gunton, William Sadler',
-  plot:
-    "Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit. The film portrays the man's unique way of dealing with his new, torturous life; along the way he befriends a number of fellow prisoners, most notably a wise long-term inmate named Red.",
-  languages: 'English',
-  country: 'USA',
-  awards: 'Nominated for 7 Oscars. Another 19 wins & 29 nominations.',
-  poster:
-    'https://ia.media-imdb.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
-  ratings: [
-    { Source: 'Internet Movie Database', Value: '9.3/10' },
-    { Source: 'Rotten Tomatoes', Value: '91%' },
-    { Source: 'Metacritic', Value: '80/100' }
-  ]
-};
-
-//needed title, imagePath, plotString, rating, actors, genres, releaseDate, rated
-
-async function insertMovie(movieObj) {
+async function insertMovie(movieObj, movieFilePath) {
   const data = {
     title: movieObj.title,
+    filePath: movieFilePath,
     imageUrl: '',
     plot: movieObj.plot,
     rating: null,
@@ -56,17 +29,9 @@ async function insertMovie(movieObj) {
   db.insert(data)
 }
 
-// insertMovie(testMovie)
-
-
-// db.insert(testData, (err, newItem) => {
-//   console.log(newItem)
-// })
-
-// db.find({title: 'world'}, (err, docs) => {
-//   console.log(docs)
-// })
-
-//db.remove({_id: 'rLn6Yu8XLtpfrqhr'})
-
 db.persistence.compactDatafile();
+
+module.exports = {
+  db,
+  insertMovie
+}
