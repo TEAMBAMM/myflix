5d99ff88a2789d9556e0c5e09abe6f92abc1cd4d
Matthew Stirnweiss <mpstirn@outlook.com>
2018-05-07 16:16:38 -0400

    Pull confilcts

--- Filereader/index.js
+++ Filereader/index.js
@@ -1,54 +1,66 @@
-const chokidar = require('chokidar')
-const path = require('path')
-const url = require('url')
+const chokidar = require('chokidar');
+const imdb = require('imdb-api');
+const path = require('path');
+const url = require('url');
+const { db, insertMovie } = require('../data/dataStore')
 
-const fileWatcher = chokidar.watch(url.format({
-  pathname: path.join(__dirname, '../','/movies'),
-  protocol: 'file',
-  slashes: true
-}), {
-  persistent: true
-})
+const fileWatcher = chokidar.watch(path.join(__dirname, '../', '/movies'), {
+  persistent: true,
+  ignored: '.DS_Store', // For mac computers
+  ignoreInitial: true, // So it does not check files already present
+});
 
-console.log('Watching: ', url.format({
-  pathname: path.join(__dirname, '../','/movies'),
-  protocol: 'file',
-  slashes: true
-}))
+console.log('Watching: ', path.join(__dirname, '../', '/movies'));
 
-fileWatcher.on('add', path => {
-  const name = extractName(path)
-  console.log(`Added: ${path}`)
-})
+fileWatcher.on('add', async path => {
+  const name = extractName(path);
+  if (db.find({title: name})) {
+    console.log('HIT: ', name)
+  } else {
+    try {
+      //Retrieve data from imdb
+      const data = await imdb.get(name, { apiKey: 'ed483961' })
+      //Custom insert movie function that downloads the poster image
+      //and creates object in database of the data we need
+      insertMovie(data, path)
+    } catch (err){
+      console.error('IMDB DATA FAILURE', err)
+    }
+    console.log(`Added: ${path}`);
+  }
+});
 
 fileWatcher.on('addDir', path => {
-  const name = extractName(path)
-  console.log(`Added: ${path}`)
-})
+  const name = extractName(path);
+  console.log(`Added Directory: ${path}`);
+});
 
 fileWatcher.on('change', path => {
-  const name = extractName(path)
-  console.log(`Changed: ${path}`)
-})
+  const name = extractName(path);
+  console.log(`Changed: ${path}`);
+});
 
 fileWatcher.on('unlink', path => {
-  const name = extractName(path)
-  console.log(`Removed: ${path}`)
-})
+  const name = extractName(path);
+  db.remove({filePath: path}, {multi: true}, function(err, fileRemoved) {
+    if (err) console.log('ERROR DURING DELETION - DB', err)
+  })
+  console.log(`Removed: ${path}`);
+});
 
 fileWatcher.on('unlinkDir', path => {
-  const name = extractName(path)
-  console.log(`Removed: ${path}`)
-})
+  const name = extractName(path);
+  console.log(`Removed Directory: ${path}`);
+});
 
 fileWatcher.on('error', error => {
-  console.log(`ERROR: ${error}`)
-})
+  console.log(`ERROR: ${error}`);
+});
 
 const extractName = path => {
-  for(let i = path.length - 1; i > 0; i--) {
+  for (let i = path.length - 1; i > 0; i--) {
     if (path[i] === '/' || path[i] === '\\') {
-      return path.slice(i + 1)
+      return path.slice(i + 1, -4);
     }
   }
-}
+};
