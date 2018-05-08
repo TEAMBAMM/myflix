637a66678333596cdc08d3e5de7fc7fce119bc7c
Alona Varshal <thirdwave72@gmail.com>
2018-05-08 15:18:37 -0500

    Add video control buttons

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -2,186 +2,265 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 import { withRouter } from 'react-router-dom';
 import screenfull from 'screenfull';
+import { findDOMNode } from 'react-dom';
+import Duration from './Duration';
+import FlatButton from 'material-ui/FlatButton';
+
+// const filename = 'StarWarsTheLastJedi.mp4';  // for testing by Blake
+// const ip = '192.168.1.12:80' // from Blake
 
 class Player extends React.Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     url: null,
-  //     playing: true,
-  //     volume: 0.5,
-  //     muted: true,
-  //     played: 0,
-  //     loaded: 0,
-  //     duration: 0,
-  //     playbackRate: 1.0,
-  //     loop: false
-  //   };
-  // }
+  constructor() {
+    super();
+    this.state = {
+      url: null,
+      playing: false,
+      muted: true,
+      volume: 0.5,
+      played: 0,
+      loaded: 0,
+      duration: 0,
+      playbackRate: 1.0
+    };
+    this.playPause = this.playPause.bind(this);
+    this.onPlay = this.onPlay.bind(this);
+    this.onPause = this.onPause.bind(this);
+    this.onClickFullscreen = this.onClickFullscreen.bind(this);
+    this.setVolume = this.setVolume.bind(this);
+    this.setPlaybackRate = this.setPlaybackRate.bind(this);
+    this.onSeekMouseDown = this.onSeekMouseDown.bind(this);
+    this.onSeekChange = this.onSeekChange.bind(this);
+    this.onSeekMouseUp = this.onSeekMouseUp.bind(this);
+    this.onProgress = this.onProgress.bind(this);
+    this.toggleMuted = this.toggleMuted.bind(this);
+    this.onDuration = this.onDuration.bind(this);
+    this.ref = this.ref.bind(this);
+    this.onEnded = this.onEnded.bind(this);
+  }
 
-  // load(url) {
-  //   this.setState({
-  //     url,
-  //     played: 0,
-  //     loaded: 0
-  //   });
-  // }
+  componentDidMount() {
+    // dummy data
+    const movieUrl = this.props.movies.filter(
+      movie => this.props.match.params.id === movie.imdbid
+    )[0].videoplayer;
 
-  // playPause() {
-  //   this.setState({
-  //     playing: !this.state.playing
-  //   });
-  // }
-
-  // setVolume(event) {
-  //   this.setState({
-  //     volume: parseFloat(event.target.value)
-  //   });
-  // }
-
-  // toggleMuted() {
-  //   this.setState({
-  //     muted: !this.state.muted
-  //   });
-  // }
-
-  // setPlaybackRate(event) {
-  //   this.setState({
-  //     playbackRate: parseFloat(event.target.value)
-  //   });
-  // }
-
-  // onPlay() {
-  //   console.log('onPlay');
-  //   this.setState({
-  //     playing: true
-  //   });
-  // }
-
-  // onPause() {
-  //   console.log('onPause');
-  //   this.setState({
-  //     playing: false
-  //   });
-  // }
-
-  // onSeekMouseDown(e) {
-  //   this.setState({
-  //     seeking: true
-  //   });
-  // }
-
-  // onSeekChange(e) {
-  //   this.setState({
-  //     played: parseFloat(e.target.value)
-  //   });
-  // }
-
-  // onSeekMouseUp(e) {
-  //   this.setState({
-  //     seeking: false
-  //   });
-  //   this.player.seekTo(parseFloat(e.target.value));
-  // }
-
-  // onProgress(state) {
-  //   console.log('onProgress', state);
-  //   // We only want to update time slider if we are not currently seeking
-  //   if (!this.state.seeking) {
-  //     this.setState(state);
-  //   }
-  // }
-
-  // onEnded() {
-  //   console.log('onEnded');
-  //   this.setState({
-  //     playing: this.state.loop
-  //   });
-  // }
-
-  // onDuration(duration) {
-  //   console.log('onDuration', duration);
-  //   this.setState({ duration });
-  // }
-
-  // onClickFullscreen() {
-  //   screenfull.request(findDOMNode(this.player));
-  // }
-
-  // renderLoadButton(url, label) {
-  //   return <button onClick={() => this.load(url)}>{label}</button>;
-  // }
-
-  // ref(player) {
-  //   this.player = player;
-  // }
-
-  render() {
-    // variables needed for actual playing
-    // const {
-    //   // url
-    //   playing,
-    //   volume,
-    //   muted,
-    //   loop,
-    //   played,
-    //   loaded,
-    //   duration,
-    //   playbackRate
-    // } = this.state;
-    // const SEPARATOR = ' . ';
-
-    const { movies } = this.props;
-    const imdbid = this.props.match.params.id;
-    const movie = movies.filter(movie => imdbid === movie.imdbid)[0];
-    const movieUrl = movie.videoplayer;
-    // const filename = 'StarWarsTheLastJedi.mp4';  // for testing by Blake
-    // const ip = '192.168.1.12:80' // from Blake
     const filename = 'farm_thxgvg_2017.mp4'; // only for testing by Alona
     const ip = '192.168.0.7'; // only for testing by Alona
-    const url = `http://${ip}/${filename}`;
+    const tempUrl = `http://${ip}/${filename}`;
+
+    this.setState({
+      url: tempUrl,
+      playing: true,
+      muted: false
+    });
+  }
+
+  playPause() {
+    this.setState({
+      playing: !this.state.playing
+    });
+    console.log(this.state.playing);
+  }
+
+  onPlay() {
+    console.log('onPlay');
+    this.setState({
+      playing: true
+    });
+  }
+
+  onPause() {
+    console.log('onPause');
+    this.setState({
+      playing: false
+    });
+  }
+
+  onClickFullscreen() {
+    screenfull.request(findDOMNode(this.player));
+  }
+
+  setVolume(e) {
+    this.setState({ volume: parseFloat(e.target.value) });
+  }
+
+  setPlaybackRate(e) {
+    this.setState({ playbackRate: parseFloat(e.target.value) });
+  }
+
+  onSeekMouseDown(e) {
+    this.setState({ seeking: true });
+  }
+  onSeekChange(e) {
+    this.setState({ played: parseFloat(e.target.value) });
+  }
+  onSeekMouseUp(e) {
+    this.setState({ seeking: false });
+    this.player.seekTo(parseFloat(e.target.value));
+  }
+  onProgress(state) {
+    console.log('onProgress', state);
+    // We only want to update time slider if we are not currently seeking
+    if (!this.state.seeking) {
+      this.setState(state);
+    }
+  }
+  toggleMuted() {
+    this.setState({ muted: !this.state.muted });
+  }
+
+  onDuration(duration) {
+    console.log('onDuration', duration);
+    this.setState({ duration });
+  }
+
+  onEnded() {
+    console.log('onEnded');
+    this.setState({ playing: this.state.loop });
+  }
+
+  ref(player) {
+    this.player = player;
+  }
+
+  render() {
+    const {
+      url,
+      playing,
+      muted,
+      volume,
+      played,
+      loaded,
+      duration,
+      playbackRate
+    } = this.state;
+
     return (
       <div className="player-container">
         <div className="player-overlay">
           <a href="#">
             <ReactPlayer
-              // ref={this.ref}
-              url={url}
-              // volume={volume}
-              // muted={muted}
-              playing
               className="react-player"
-              // onReady={() => console.log('onReady')}
-              // onStart={() => console.log('onStart')}
-              // onPlay={this.onPlay}
-              // onPause={this.onPause}
-              // onBuffer={() => console.log('onBuffer')}
-              // onSeek={e => console.log('onSeek', e)}
-              // onEnded={this.onEnded}
-              // onError={e => console.log('onError', e)}
-              // onProgress={this.onProgress}
-              // onDuration={this.onDuration}
-              controls={true}
               width="100%" // cannot be put on css, must be in here, I found
               height="100%"
+              url={url}
+              playing={playing}
+              onPlay={this.onPlay}
+              onPause={this.onPause}
+              muted={muted}
+              volume={volume}
+              onReady={() => console.log('onReady')}
+              onStart={() => console.log('onStart')}
+              onPlay={this.onPlay}
+              onPause={this.onPause}
+              onBuffer={() => console.log('onBuffer')}
+              onSeek={e => console.log('onSeek', e)}
+              onEnded={this.onEnded}
+              onError={e => console.log('onError', e)}
+              onProgress={this.onProgress}
+              onDuration={this.onDuration}
             />
           </a>
-          {/* <a href="#" className="controlWrapper">
+          {/*
+          Attempt at doing hovering css futile for now
+          <a href="#" className="controlWrapper">
             <span className="controlBtns">
               <div className="center-controls">
-                <button>Back</button>
-                <button>FastBack</button>
-                <button onClick={() => this.playPause}>
+                <button onClick={() => this.playPause()}>
                   {playing ? 'Pause' : 'Play'}
                 </button>
-                <button>FastForward</button>
-                <button>Forward</button>
               </div>
               <div className="left-controls">
+                <input
+                  type="range"
+                  min={0}
+                  max={1}
+                  step="any"
+                  value={volume}
+                  onChange={() => this.setVolume()}
+                />
                 <button onClick={this.onClickFullscreen}>Fullscreen</button>
               </div>
             </span>
           </a> */}
+
+          <div className="table">
+            <table>
+              <tbody>
+                <tr>
+                  <th>Controls</th>
+                  <td>
+                    <button onClick={this.playPause}>
+                      {playing ? 'Pause' : 'Play'}
+                    </button>
+                    <button onClick={this.onClickFullscreen}>Fullscreen</button>
+                    <button onClick={this.setPlaybackRate} value={1}>
+                      1
+                    </button>
+                    <button onClick={this.setPlaybackRate} value={1.5}>
+                      1.5
+                    </button>
+                    <button onClick={this.setPlaybackRate} value={2}>
+                      2
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <th>Seek</th>
+                  <td>
+                    <input
+                      type="range"
+                      min={0}
+                      max={1}
+                      step="any"
+                      value={played}
+                      onMouseDown={this.onSeekMouseDown}
+                      onChange={this.onSeekChange}
+                      onMouseUp={this.onSeekMouseUp}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <th>Volume</th>
+                  <td>
+                    <input
+                      type="range"
+                      min={0}
+                      max={1}
+                      step="any"
+                      value={volume}
+                      onChange={this.setVolume}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <th>
+                    <label htmlFor="muted">Muted</label>
+                  </th>
+                  <td>
+                    <input
+                      id="muted"
+                      type="checkbox"
+                      checked={muted}
+                      onChange={this.toggleMuted}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <th>Played</th>
+                  <td>
+                    <progress max={1} value={played} />
+                  </td>
+                </tr>
+                <tr>
+                  <th>Loaded</th>
+                  <td>
+                    <progress max={1} value={loaded} />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     );
