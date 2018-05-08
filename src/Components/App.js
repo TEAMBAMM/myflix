705eeac4f24ad52c14ef1cc464a0f0a46115ab80
Matthew Bestard <matthew.r.bestard@gmail.com>
2018-05-08 17:14:06 -0400

    add ability to sort movie list by categories in sort menu

--- src/Components/App.js
+++ src/Components/App.js
@@ -20,7 +20,7 @@ class App extends Component {
       searchInput: '',
       isPlaying: false,
       filter: 'All',
-      sort: 'Recently Added',
+      sort: 'Title',
       currentMoviePosition: '',
       favorites: false,
       filteredOutput: []
@@ -31,12 +31,13 @@ class App extends Component {
     this.toggleFavorites = this.toggleFavorites.bind(this);
   }
 
-  componentDidMount() {
-    this.setState({
+  async componentDidMount() {
+    await this.setState({
       ...this.state,
       movies: movieArray,
       filteredOutput: movieArray
     });
+    await this.updateSortedList();
   }
 
   async toggleFavorites(event) {
@@ -55,16 +56,19 @@ class App extends Component {
     event.preventDefault();
     await this.setState({ ...this.state, filter: value });
     this.updateSortedList();
+    console.log(await this.state);
   }
 
-  changeSort(event, value) {
+  async changeSort(event, value) {
     event.preventDefault();
-    this.setState({ ...this.state, sort: value });
+    await this.setState({ ...this.state, sort: value });
+    this.updateSortedList();
   }
 
   async updateSortedList() {
-    let moviesList = this.state.movies;
-    let filterTerm = this.state.filter;
+    let moviesList = this.state.movies; // movies array from state
+    let filterTerm = this.state.filter; // term to sort genre by
+    let sortTerm = this.state.sort; // term to sort category by
     let filteredOutput;
     if (filterTerm !== 'All') {
       filteredOutput = moviesList.filter(movie => {
@@ -72,6 +76,31 @@ class App extends Component {
       });
     } else {
       filteredOutput = moviesList;
+    }
+    switch (sortTerm) {
+      case 'Recently Added': // will update later
+        break;
+      case 'Title':
+        filteredOutput.sort((movieA, movieB) => {
+          const movieAL = movieA.title.toLowerCase();
+          const movieBL = movieB.title.toLowerCase();
+          if (movieAL < movieBL) return -1;
+          if (movieAL > movieBL) return 1;
+          return 0;
+        });
+        break;
+      case 'Rating':
+        filteredOutput.sort((movieA, movieB) => {
+          return movieB.rating - movieA.rating;
+        });
+        break;
+      case 'Year':
+        filteredOutput.sort((movieA, movieB) => {
+          return movieB.year - movieA.year;
+        });
+        break;
+      case 'Resolution': // will update later
+        break;
     }
     await this.setState({ ...this.state, filteredOutput: filteredOutput });
   }
