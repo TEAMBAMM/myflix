d53c0cfa0b06a96cbe7862bfb2ab6a3bf962cfab
Alona Varshal <thirdwave72@gmail.com>
2018-05-08 15:36:17 -0500

    Refactor Player component

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -5,6 +5,7 @@ import screenfull from 'screenfull';
 import { findDOMNode } from 'react-dom';
 import Duration from './Duration';
 import FlatButton from 'material-ui/FlatButton';
+import VideoControls from './VideoControls';
 
 // const filename = 'StarWarsTheLastJedi.mp4';  // for testing by Blake
 // const ip = '192.168.1.12:80' // from Blake
@@ -161,106 +162,20 @@ class Player extends React.Component {
               onDuration={this.onDuration}
             />
           </a>
-          {/*
-          Attempt at doing hovering css futile for now
-          <a href="#" className="controlWrapper">
-            <span className="controlBtns">
-              <div className="center-controls">
-                <button onClick={() => this.playPause()}>
-                  {playing ? 'Pause' : 'Play'}
-                </button>
-              </div>
-              <div className="left-controls">
-                <input
-                  type="range"
-                  min={0}
-                  max={1}
-                  step="any"
-                  value={volume}
-                  onChange={() => this.setVolume()}
-                />
-                <button onClick={this.onClickFullscreen}>Fullscreen</button>
-              </div>
-            </span>
-          </a> */}
-
-          <div className="table">
-            <table>
-              <tbody>
-                <tr>
-                  <th>Controls</th>
-                  <td>
-                    <button onClick={this.playPause}>
-                      {playing ? 'Pause' : 'Play'}
-                    </button>
-                    <button onClick={this.onClickFullscreen}>Fullscreen</button>
-                    <button onClick={this.setPlaybackRate} value={1}>
-                      1
-                    </button>
-                    <button onClick={this.setPlaybackRate} value={1.5}>
-                      1.5
-                    </button>
-                    <button onClick={this.setPlaybackRate} value={2}>
-                      2
-                    </button>
-                  </td>
-                </tr>
-                <tr>
-                  <th>Seek</th>
-                  <td>
-                    <input
-                      type="range"
-                      min={0}
-                      max={1}
-                      step="any"
-                      value={played}
-                      onMouseDown={this.onSeekMouseDown}
-                      onChange={this.onSeekChange}
-                      onMouseUp={this.onSeekMouseUp}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <th>Volume</th>
-                  <td>
-                    <input
-                      type="range"
-                      min={0}
-                      max={1}
-                      step="any"
-                      value={volume}
-                      onChange={this.setVolume}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <th>
-                    <label htmlFor="muted">Muted</label>
-                  </th>
-                  <td>
-                    <input
-                      id="muted"
-                      type="checkbox"
-                      checked={muted}
-                      onChange={this.toggleMuted}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <th>Played</th>
-                  <td>
-                    <progress max={1} value={played} />
-                  </td>
-                </tr>
-                <tr>
-                  <th>Loaded</th>
-                  <td>
-                    <progress max={1} value={loaded} />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          <VideoControls
+            playPause={this.playPause}
+            onClickFullscreen={this.onClickFullscreen}
+            setPlaybackRate={this.setPlaybackRate}
+            onSeekMouseDown={this.onSeekMouseDown}
+            onSeekChange={this.onSeekChange}
+            onSeekMouseUp={this.onSeekMouseUp}
+            volume={volume}
+            muted={muted}
+            toggleMuted={this.toggleMuted}
+            played={played}
+            loaded={loaded}
+            playing={playing}
+          />
         </div>
       </div>
     );
