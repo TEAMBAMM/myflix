1b7b4b0c12eedccf8d114677f568401f59cec3ee
Alona Varshal <thirdwave72@gmail.com>
2018-05-07 23:05:18 -0500

    CSS formatting (not done)

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -8,30 +8,37 @@ const Player = props => {
   const movie = movies.filter(movie => imdbid === movie.imdbid)[0];
   const movieUrl = movie.videoplayer;
   // const filename = 'StarWarsTheLastJedi.mp4';
+  const filename = 'farm_thxgvg_2017.mp4';
+  const ip = '192.168.0.7';
 
   return (
     <div className="player-container">
       <div className="player-overlay">
         <a href="#">
           <ReactPlayer
-            url={movieUrl}
-            controls={true}
-            volume={1}
-            muted={false}
-            playing
+            // url={movieUrl}
+            url={`http://${ip}/${filename}`}
+            // controls={true}
+            // volume={1}
+            // muted={true}
+            // playing
             className="react-player"
             width="100%"
             height="100%"
           />
         </a>
-        <a href="#" className="controls-wrapper">
-          <span className="controlsBtn">
-            <button>Back</button>
-            <button>FastBack</button>
-            <button>Play/Stop</button>
-            <button>FastForward</button>
-            <button>FastForward</button>
-            <button>Fullscreen</button>
+        <a href="#" className="controlWrapper">
+          <span className="controlBtns">
+            <div className="center-controls">
+              <button>Back</button>
+              <button>FastBack</button>
+              <button>Play/Stop</button>
+              <button>FastForward</button>
+              <button>FastForward</button>
+            </div>
+            <div className="left-controls">
+              <button>Fullscreen</button>
+            </div>
           </span>
         </a>
       </div>
