99696a41625a8693a880f668b347e5e5dae8d38e
Matthew Stirnweiss <mpstirn@outlook.com>
2018-05-10 13:57:16 -0400

    Promises working

--- Filereader/index.js
+++ Filereader/index.js
@@ -1,97 +1,66 @@
 const fs = require('fs');
 const chokidar = require('chokidar');
-const imdb = require('imdb-api');
 const path = require('path');
 const url = require('url');
-const { db, insertMovie } = require('../data/dataStore');
+const {
+  db,
+  insertMovie,
+  removeMovie,
+  // promisifiedCount,
+  // promisifiedFind,
+  // promisifiedRemove
+} = require('../data/dataStore');
 
-const fileWatcher = chokidar.watch(path.join(__dirname, '../', '/movies'), {
-  persistent: true,
-  ignored: /(^|[/\\])\../ //For .DS_Store on MacOS
-});
+const fileWatcher = chokidar.watch(
+  path.join(__dirname, '../', '/movies'), {
+    persistent: true,
+    ignored: /(^|[/\\])\../ //For .DS_Store on MacOS
+  }
+);
 
-db.persistence.setAutocompactionInterval(5000);
+db.persistence.setAutocompactionInterval(10000);
 
-fileWatcher.on('add', filePath => {
-  const { name } = path.parse(filePath);
-  db.findOne({ title: name }, async (err, doc) => {
-    if (doc === null) {
-      try {
-        //Retrieve data from imdb
-        const data = await imdb.get(name, { apiKey: 'ed483961' });
-        //Custom insert movie function that downloads the poster image
-        //and creates object in database of the data we need
-        insertMovie(data, filePath);
-      } catch (err) {
-        console.error('IMDB DATA FAILURE', err);
-      }
-    }
-  });
-});
+fileWatcher.on('add', filePath => insertMovie(filePath));
 
-fileWatcher.on('unlink', filePath => {
-  const { name } = path.parse(filePath);
-  // Remove movie from database
-  db.remove({ filePath }, { multi: true }, function(err, fileRemoved) {
-    if (err) console.log('ERROR DURING DELETION - DB', err);
-  });
-  // Remove movie image from poster folder
-  fs.unlink(
-    path.join(__dirname, '../data/moviePosters', `${name}-poster.jpg`),
-    err => {
-      if (err) console.error(err);
-    }
-  );
-});
+fileWatcher.on('unlink', filePath => removeMovie(filePath));
 
-fileWatcher.on('error', error => {
-  console.log(`ERROR: ${error}`);
-});
+fileWatcher.on('error', error => console.log(`ERROR: ${error}`));
 
-fileWatcher.on('ready', () => {
-  // Retrieve files being watched
-  let watched = fileWatcher.getWatched();
-  // Extract just our movies file array
-  watched = watched[path.join(__dirname, '../movies')];
-  // Remove ext name, leaving an array of movie title
-  // currently in the directory
-  let watching = watched.map(movieName => path.parse(movieName)['name']);
-  // Get count of entries in the datastore
-  db.count({}, (err, dbEntryCount) => {
-    // If less, movie(s) were added while the application was closed
-    if (dbEntryCount < watching.length) {
-      watching.forEach((movieTitle, movieIndex) => {
-        // TODO What if a duplicate is added while app is closed?
-        // Double checking for duplicates
-        db.findOne({ title: movieTitle }, (err, doc) => {
-          if (err) console.error(err);
-          if (doc === null) {
-            try {
-              //Retrieve data from imdb
-              const data = imdb.get(movieTitle, { apiKey: 'ed483961' }).then(data => {
-                //Custom insert movie function that downloads the poster image
-                //and creates object in database of the data we need
-                insertMovie(data, watched[movieIndex]);
-              });
-            } catch (err) {
-              console.error('IMDB DATA FAILURE', err);
-            }
-          }
-        });
-      });
-    } else if (dbEntryCount > watching.length) {
-      // If there are more entries in the database then in the file,
-      // delete the database entries
-      let dbEntries = db.find({}, (err, dbEntries) => {
-        // Loop through entries in the database
-        dbEntries.forEach(entry => {
-          let entryFileName = entry.fileName;
-          // If it is not being watched, it is removed from the database
-          if (!watching.includes(entryFileName)) {
-            db.remove({ title: entryFileName });
-          }
-        });
-      });
-    }
-  });
-});
+// fileWatcher.on('ready', async () => {
+//   // Retrieve files being watched
+//   let watched = fileWatcher.getWatched();
+//   // Extract just our movies file array
+//   watched = watched[path.join(__dirname, '../movies')];
+//   // Remove ext name, leaving an array of movie title
+//   // currently in the directory
+//   let watching = watched.map(movieName => path.parse(movieName)['name']);
+//   // Get count of entries in the datastore
+//   let dbEntryCount = await promisifiedCount()
+//   // If less, movie(s) were added while the application was closed
+//   if (dbEntryCount < watching.length) {
+//     (async function watchingLoop() {
+//       for(let i = 0; i < watching.length; i++) {
+//         await insertMovie
+//       }
+//     })()
+//     // watching.reduce( (p, _, i) => {
+//     //   p.then(_ => insertMovie(data, watched[i]))
+//     // }, promise.resolve() );
+//   } else if (dbEntryCount > watching.length) {
+//     // If there are more entries in the database then in the file,
+//     // delete the database entries
+//     let dbEntries = await promisifiedFind({})
+//     // Loop through entries in the database
+//     dbEntries.reduce( (p, entry, i) => {
+//       let entryFileName = entry.fileName;
+//       if (!watching.includes(entryFileName)) {
+//         promiedRemove({ title: entryFileName });
+//       }
+//     }, promise.resolve())
+
+//     dbEntries.forEach(entry => {
+//       // If it is not being watched, it is removed from the database
+//     });
+    
+//   }
+// });
--- data/dataStore.js
+++ data/dataStore.js
@@ -1,45 +1,102 @@
 const Datastore = require('nedb');
 const path = require('path');
+const imdb = require('imdb-api');
 const download = require('image-downloader');
-console.log('DATA DIR', __dirname)
+console.log('DATA DIR', __dirname);
 const db = new Datastore({
   filename: path.join(__dirname, '/databaseStorage'),
   autoload: true
 });
 
-async function insertMovie(movieObj, movieFilePath) {
-  const data = {
-    title: movieObj.title,
-    year: movieObj.year,
-    imdbid: movieObj.imdbid,
-    filePath: movieFilePath,
-    imageUrl: '',
-    plot: movieObj.plot,
-    rating: null,
-    rated: movieObj.rated,
-    released: movieObj.released
+function promisiedCount() {
+  return new Promise((resolve, reject) => {
+    db.count({}, (err, count) => {
+      err ? reject(err) : resolve(count);
+    });
+  });
+}
+
+function promisiedRemove(query, options) {
+  return new Promise((resolve, reject) => {
+    db.remove(query, (err, docs) => {
+      err ? reject(err) : resolve(docs);
+    });
+  });
+}
+
+function promisiedFind(query, options) {
+  return new Promise((resolve, reject) => {
+    db.find(query, (err, docs) => {
+      err ? reject(err) : resolve(docs);
+    });
+  });
+}
+
+function promisiedFindOne(query, options) {
+  return new Promise((resolve, reject) => {
+    db.findOne(query, (err, doc) => {
+      err ? reject(err) : resolve(doc);
+    });
+  });
+}
+
+async function insertMovie(movieFilePath) {
+  const { name } = path.parse(movieFilePath);
+  // TODO What if a duplicate is added while app is closed?
+  // Double checking for duplicates
+  const doc = await promisiedFindOne({ title: name });
+  if (doc === null) {
+    //Retrieve data from imdb
+    const imdbData = await imdb.get(name, { apiKey: 'ed483961' });
+    //Download poster image from imdb
+    try {
+      download.image({
+        url: imdbData.poster,
+        dest: `data/moviePosters/${imdbData.title}-poster.jpg`
+      });
+    } catch (e) {
+      console.log('Image Not Loaded');
+    }
+    let parsedPath = path.parse(movieFilePath);
+    const data = {
+      title: imdbData.title,
+      year: imdbData.year,
+      imdbid: imdbData.imdbid,
+      plot: imdbData.plot,
+      rating: null,
+      rated: imdbData.rated,
+      released: imdbData.released,
+      dateAdded: new Date(),
+      genres: imdbData.genres.split(', '),
+      actors: imdbData.actors.split(', '),
+      baseFileName: parsedPath.base,
+      fileType: parsedPath.ext,
+      fileName: parsedPath.name
+    };
+    return new Promise((resolve, reject) => {
+      db.insert(data, err => {
+        err ? reject(err) : resolve();
+      });
+    });
   }
-  let parsedPath = path.parse(movieFilePath);
-  data.genres = movieObj.genres.split(', ');
-  data.actors = movieObj.actors.split(', ');
-  data.baseFileName = parsedPath.base;
-  data.fileType = parsedPath.ext;
-  data.fileName = parsedPath.name;
-  try {
-    const {imageFilename} = await download.image({
-      url: movieObj.poster,
-      dest: `data/moviePosters/${data.title}-poster.jpg`
-    })
-    data.imageUrl = imageFilename
-    db.insert(data)
-  } catch (e) {
-    const imageFilename = path.join(__dirname, 'moviePosters/sad-face.jpg')
-    data.imageUrl = imageFilename
-    db.insert(data)
-  }
+}
+
+async function removeMovie(movieFilePath) {
+  const { name } = path.parse(filePath);
+  // Remove movie image from poster folder
+  await fs.unlink(
+    path.join(__dirname, '../data/moviePosters', `${name}-poster.jpg`),
+    err => {
+      if (err) console.error(err);
+    }
+  );
+  // Remove movie from database
+  await promisiedRemove({ filePath }, { multi: true });
+  
 }
 
 module.exports = {
   db,
-  insertMovie
-}
+  insertMovie,
+  removeMovie
+};
