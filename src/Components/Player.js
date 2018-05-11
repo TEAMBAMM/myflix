9096825cfbe1d51231da884051d68d36fde54174
Alona Varshal <thirdwave72@gmail.com>
2018-05-11 05:59:49 -0500

    Format volume controls

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -20,11 +20,8 @@ class Player extends React.Component {
       playbackRate: 1.0
     };
     this.playPause = this.playPause.bind(this);
-    // this.onPlay = this.onPlay.bind(this);
-    // this.onPause = this.onPause.bind(this);
     this.onClickFullscreen = this.onClickFullscreen.bind(this);
     this.setVolume = this.setVolume.bind(this);
-    // this.setPlaybackRate = this.setPlaybackRate.bind(this);
     this.onSeekMouseDown = this.onSeekMouseDown.bind(this);
     this.onSeekChange = this.onSeekChange.bind(this);
     this.onSeekMouseUp = this.onSeekMouseUp.bind(this);
@@ -32,20 +29,15 @@ class Player extends React.Component {
     this.toggleMuted = this.toggleMuted.bind(this);
     this.onDuration = this.onDuration.bind(this);
     this.ref = this.ref.bind(this);
-    // this.onEnded = this.onEnded.bind(this);
     this.forward = this.forward.bind(this);
     this.back = this.back.bind(this);
     this.increaseVolume = this.increaseVolume.bind(this);
     this.decreaseVolume = this.decreaseVolume.bind(this);
-    // this.onMouseOver = this.onMouseOver.bind(this);
-    // this.onMouseMove = this.onMouseMove.bind(this);
   }
 
   componentDidMount() {
     const { ip, movies, match } = this.props;
     const movie = movies.filter(movie => match.params.id === movie.imdbid)[0];
-
-    //  'Den of Thieves'
     const fileName = movie.fileName;
 
     // Right now ip is just of your local machine - need to figure out
@@ -53,7 +45,6 @@ class Player extends React.Component {
     // const ip = movie ? movie.ip : 'localhost';
     const ip_ = ip ? ip : localhost;
 
-    // 'Den of Thieves.mkv'
     const baseFileName = movie.baseFileName;
 
     this.setState({
@@ -79,56 +70,58 @@ class Player extends React.Component {
   }
 
   forward() {
-    const playedIncrement = 0.02;
+    const playedIncrement = 0.01;
     if (this.state.played + playedIncrement <= 1) {
       this.player.seekTo(this.state.played + playedIncrement);
     }
   }
 
   back() {
-    const playedIncrement = 0.02;
+    const playedIncrement = 0.01;
     if (this.state.played - playedIncrement >= 0) {
       this.player.seekTo(this.state.played - playedIncrement);
     }
   }
 
   decreaseVolume() {
-    const volumeIncrement = 0.1;
+    const volumeIncrement = 0.005;
     if (this.state.volume - volumeIncrement >= 0) {
       this.setState({
-        volume: this.state.volume - 0.1
+        volume: this.state.volume - volumeIncrement
       });
     }
-    console.log(this.state.volume);
   }
 
   increaseVolume() {
-    const volumeIncrement = 0.1;
+    const volumeIncrement = 0.005;
     if (this.state.volume + volumeIncrement <= 1) {
       this.setState({
         volume: this.state.volume + volumeIncrement
       });
     }
-    console.log(this.state.volume);
   }
 
   onSeekMouseDown(e) {
     this.setState({ seeking: true });
   }
+
   onSeekChange(e) {
     this.setState({ played: parseFloat(e.target.value) });
   }
+
   onSeekMouseUp(e) {
     this.setState({ seeking: false });
     this.player.seekTo(parseFloat(e.target.value));
   }
+
+  // We need this function, don't remove it!  Without this, the back and forward buttons will not work. Also, if the seekTo function (built in for ReactPlayer component) is not active (turns this.state.seeking on), setting of state is not happening. So I don't think this is the reason for the memory leak.
   onProgress(state) {
-    console.log('onProgress', state);
     // We only want to update time slider if we are not currently seeking
     if (!this.state.seeking) {
       this.setState(state);
     }
   }
+
   toggleMuted() {
     this.setState({ muted: !this.state.muted });
   }
@@ -189,6 +182,7 @@ class Player extends React.Component {
               decreaseVolume={this.decreaseVolume}
               increaseVolume={this.increaseVolume}
               ip={ip}
+              duration={duration}
             />
           </div>
         </div>
--- src/Components/VideoControls.js
+++ src/Components/VideoControls.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+function precisionRound(number, precision) {
+  var factor = Math.pow(10, precision);
+  return Math.round(number * factor) / factor;
+}
+
 const VideoControls = ({
   playPause,
   onClickFullscreen,
@@ -10,29 +15,32 @@ const VideoControls = ({
   muted,
   toggleMuted,
   played,
-  loaded,
   setVolume,
   playing,
   forward,
   back,
   increaseVolume,
   decreaseVolume,
-  ip
+  ip,
+  duration
 }) => {
   const ip_ = ip ? ip : 'localhost';
   return (
     <div className="controls">
-      <input
-        className="seekbar"
-        type="range"
-        min={0}
-        max={1}
-        step="any"
-        value={played}
-        onMouseDown={onSeekMouseDown}
-        onChange={onSeekChange}
-        onMouseUp={onSeekMouseUp}
-      />
+      <div className="progress-bar">
+        <input
+          className="seekbar"
+          type="range"
+          min={0}
+          max={1}
+          step="any"
+          value={played}
+          onMouseDown={onSeekMouseDown}
+          onChange={onSeekChange}
+          onMouseUp={onSeekMouseUp}
+        />
+        <span>{precisionRound(duration * played, 2)}</span>
+      </div>
       <div className="play-layer">
         <div className="back" onClick={back}>
           <img src={`http://${ip_}/back.png`} width="30" height="30" alt="" />
@@ -66,7 +74,17 @@ const VideoControls = ({
         </div>
       </div>
       <div className="volume-layer">
-        <div className="volcon">
+        <div className="muted" onClick={toggleMuted}>
+          {
+            <img
+              src={`http://${ip_}/mute-volume.png`}
+              width="20"
+              height="20"
+              alt=""
+            />
+          }
+        </div>
+        <div className="trio">
           <img
             src={`http://${ip_}/low-volume.png`}
             width="20"
@@ -89,38 +107,17 @@ const VideoControls = ({
             alt=""
             onClick={increaseVolume}
           />
-          <div className="muted" onClick={toggleMuted}>
-            {
-              <img
-                src={`http://${ip_}/mute-volume.png`}
-                width="20"
-                height="20"
-                alt=""
-              />
-            }
-          </div>
         </div>
-        {/* <div className="seek">
-          <p>Seek</p>
-          <input
-            className="seekbar"
-            type="range"
-            min={0}
-            max={1}
-            step="any"
-            defaultValue={played}
-            onMouseDown={onSeekMouseDown}
-            onChange={onSeekChange}
-            onMouseUp={onSeekMouseUp}
+
+        <div className="fullscreen">
+          <img
+            src={`http://${ip_}/fullscreen.png`}
+            width="25"
+            height="25"
+            alt=""
+            onClick={onClickFullscreen}
           />
-        </div> */}
-        <img
-          src={`http://${ip_}/fullscreen.png`}
-          width="25"
-          height="25"
-          alt=""
-          onClick={onClickFullscreen}
-        />
+        </div>
       </div>
     </div>
   );
