331a8fa64f7ce5d3817412125af8714e2e7fb472
Blake Zeiger <blakezeiger@gmail.com>
2018-05-15 16:15:36 -0400

    working on no file selected

---
+++ src/Components/NoFile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NoFile = props => {
+
+  const { changeFilePath } = props
+
+  return (
+    <div className='NoFileContainer'>
+    NO FILES
+    </div>
+  )
+}
+
+export default NoFile
