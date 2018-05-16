cde09baa0f671d87ea384332c51e9ca640150b87
Blake Zeiger <blakezeiger@gmail.com>
2018-05-16 11:22:46 -0400

    working

--- src/server/index.js
+++ src/server/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 // app.use(morgan('dev'))
 
-app.use(express.static(path.join(__dirname, '../', '/movies')))
+app.use(express.static(path.resolve(settingsStore.get('movieFilePath'))))
 app.use(express.static(path.join(__dirname, '../', '/images')))
 app.use(express.static(path.join(__dirname, '../', '/data/moviePosters')))
 
